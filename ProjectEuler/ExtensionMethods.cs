28cb06d8da1c6ea009959599dcc07207fc65120c
Alfred <alfred.r.aquino@gmail.com>
2018-11-05 23:18:21 -0500

    !

---
+++ ProjectEuler/ExtensionMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectEuler
+{
+    public static partial class ExtensionMethods
+    {
+        public static string With(this string str, params object[] values)
+        {
+            return string.Format(str, values);
+        }
+
+        public static void Print(this string str) => Console.Write(str);
+
+        public static void ToConsole(this string str) => Console.WriteLine(str);
+
+        public static void KeyPrompt(this string prompt)
+        {
+            prompt.Print();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ProjectEuler/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using static System.Environment;
+
+namespace ProjectEuler
+{
+    public class App
+    {
+        public App()
+        {
+
+        }
+
+
+        public void Go()
+        {
+            $"Hello World! The time is {DateTime.Now.ToShortTimeString()}".ToConsole();
+
+            NewLine.ToConsole();
+        
+            string answer = (4.IsMultipleOf(2)) ? "is" : "is not";
+            $"4 {answer} a multiple of 2".ToConsole();
+
+            "Press any key to continue".KeyPrompt();
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new App().Go();
+        }
+    }
+}
