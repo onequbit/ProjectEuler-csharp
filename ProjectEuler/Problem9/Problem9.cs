73fd15ed1283f8cf98e319e6329247e9da69a9c3
Alfred R Aquino <araquino@developer.com>
2018-12-21 16:37:12 -0500

    work in progress

--- ProjectEuler/Problem9/Problem9.cs
+++ ProjectEuler/Problem9/Problem9.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 using CodeLibrary;
 
 namespace ProjectEuler
@@ -21,16 +22,52 @@ namespace ProjectEuler
     public class Problem9
     {
         public string Answer => _getOutput();
-        public Problem9()
-        {
-            "*Problem 9*".ToConsole();
-        }
-
         private string _getOutput()
         {
             return "";
         }
 
+        private int _a = 3;
+        private int _b = 4;
+        private int _c = 5;
+        
+        private bool IsPythagoreanTriple(int a, int b, int c)
+        {
+            return ((a * a) + (b * b)) == (c * c);
+        }
 
+        private bool IsSolution(int a, int b, int c)
+        {
+            return IsPythagoreanTriple(a, b, c) && (a + b + c == 1000);
+        }
+
+        private int[] _numbers => Enumerable.Range(1, 1000).ToArray();
+        private int[] _squares => _numbers.Select(x => x * x).ToArray();
+        
+
+        public Problem9()
+        {
+            "*Problem 9*".ToConsole();
+            
+        }
+
+        private void FindSolution()
+        {
+            List<int> pool = _numbers.ToList();
+
+
+        }
+
+        private List<int> FilterBySum(List<int> pool)
+        {
+            
+        }
+
+
+    }
+
+    public static partial class ExtensionMethods
+    {
+        
     }
 }
